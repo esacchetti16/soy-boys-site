faa8f61eb81af96cb8551a0556588533bc217119
Your Name <you@example.com>
2026-03-21 13:34:41 -0500

    Update footer contact and add flame SVG background

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@
     <footer style={{padding:24,marginTop:36,background:'#fff7e6'}}>
       <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
         <div>&copy; 2026 Soy Boys</div>
-        <div>Contact: hello@soyboyskitchen.com</div>
+        <div>Contact: admin@sous-tech.com</div>
       </div>
     </footer>
-  )
+  );
 }
